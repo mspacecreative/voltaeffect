2e40021a54c6d62a4c41b87fc0a442ab79bdfd2d
Matt <matt@mspacecreative.com>
2021-05-10 10:29:30 -0300

    c

--- includes/loops/loop-flexbox-filter.php
+++ includes/loops/loop-flexbox-filter.php
@@ -23,7 +23,7 @@
 	<div class="row">
 		
 		<?php while ( have_posts() ) : the_post();
-		$featuredimg = get_post_thumbnail('logo-grid');
+		$featuredimg = get_the_post_thumbnail('logo-grid');
 		$defaultimg = get_template_directory_uri() . '/img/placeholders/volta-blog-featured-img'; ?>
 		<div class="col col-lg-4 col-md-4 col-sm-6 col-xs-6">
 			
